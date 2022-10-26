229786847cc1aa32bb3a47709ce21794b1aeec6a
Jan Kuri <jkuri88@gmail.com>
2022-10-26 12:51:36 +0200

    chore(deps): update deps

--- src/app/shared/helpers.ts
+++ src/app/shared/helpers.ts
@@ -1,6 +1,7 @@
 import { scaleLinear } from 'd3';
 
 export const degToRad = (deg: number): number => (deg * Math.PI) / 180;
+
 export const scale = (value: number, to: number): number => {
   const sl = scaleLinear().range([0, 1]).domain([0, to]);
   return sl(value);
